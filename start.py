475764c106e3dc5f668f4015bc4ade25ae97342e
sogastar <soga.star@gmail.com>
2019-11-25 18:31:56 +0100

    Snake game in Python

---
+++ start.py
@@ -0,0 +1,17 @@
+import turtle
+wn= turtle.Screen()
+wn.title("@sogastar")
+wn.bgcolor("black")
+wn.setup(width=800, height=400)
+wn.tracer(0)
+
+
+
+
+
+
+
+
+#main game loop
+while True:
+    wn.update()
